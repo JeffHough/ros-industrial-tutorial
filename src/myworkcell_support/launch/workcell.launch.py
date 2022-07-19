8e0bdff90d293e24d8e802d361c16c5d5234d66c
Jeff H <hough552@gmail.com>
2022-07-19 17:46:23 -0400

    added launch file arguments

--- src/myworkcell_support/launch/workcell.launch.py
+++ src/myworkcell_support/launch/workcell.launch.py
@@ -13,4 +13,11 @@ def generate_launch_description():
         package='myworkcell_core',
         executable='vision_node',
     ),
+  launch_ros.actions.Node(
+      name='myworkcell_node',
+      package='myworkcell_core',
+      executable='myworkcell_node',
+      output='screen',
+      parameters=[{'base_frame': 'world'}],
+  ),
   ])
